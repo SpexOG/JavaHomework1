c3c761c11090078b0ee7679332c33c9f6685a30f
Sebastian Mutulescu <sebastian.mutulescu9@gmail.com>
2024-08-07 22:33:44 +0300

    Exercitiul 2 din ppt

---
+++ Ex2.java
@@ -0,0 +1,17 @@
+public class Ex2 {
+    public static void main(String[] args){
+
+        int a;
+        double b;
+        char c;
+
+        String d;
+
+        a = 9;
+        b = 18.9d;
+        c = 'S';
+        d = "Sebastian";
+
+        System.out.println(a + " " + b + " " + c + " " + d);
+    }
+}
--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Sebastian");
-        System.out.println("22 de ani");
-        System.out.println("Rosu");
-    }
-}
