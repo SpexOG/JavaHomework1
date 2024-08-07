5171ba273b17dde44f3cd6449ed611632213122e
Sebastian Mutulescu <sebastian.mutulescu9@gmail.com>
2024-08-07 22:51:43 +0300

    Exercitiul 2 din docx

---
+++ Ex2.java
@@ -0,0 +1,13 @@
+public class Ex2 {
+    public static void main(String[] args) {
+
+        int nr1 = 78;
+        float nr2 = 3.14f;
+
+        System.out.println("Adunare numere: " + (nr1 + nr2));
+        System.out.println("Scadere numere: " + (nr1 - nr2));
+        System.out.println("Inmultire numere: " + (nr1 * nr2));
+        System.out.println("Impartire numere: " + (nr1 / nr2));
+        System.out.println("Modulo numere: " + (nr1 % nr2));
+    }
+}
